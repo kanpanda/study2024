030f63fd49d9bf4849177c3f56a1002ef54ea4ed
NKS <iscream433@gmail.com>
2024-06-29 09:57:20 +0900

    追加:bookDaoの作成

--- bookapp/src/main/java/com/bookapp/bookapp/dao/BookDao.java
+++ bookapp/src/main/java/com/bookapp/bookapp/dao/BookDao.java
@@ -1,11 +1,23 @@
 package com.bookapp.bookapp.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bookapp.bookapp.entity.Book;
 
 @Repository
-// JpaRepository<操作対象のEntity, 操作対象Entityの主キーの型>
-public interface BookDao extends JpaRepository<Book, String>{
+public interface BookDao extends JpaRepository<Book, Long> {
+
+    // 全ての書籍を表示するメソッド
+    List<Book> findAll();
+    
+    // タイトルで書籍を検索するメソッド
+    List<Book> findByTitle(String title);
+
+    // 著者で書籍を検索するメソッド
+    List<Book> findByAuthor(String author);
+
+    // その他、必要に応じて他の検索メソッドを追加できます
 }
